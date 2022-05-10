f68c1f5312353d67072971340e0901f88dbc23b9
Phadimane Lerefolo <u19081082@tuks.co.za>
2022-05-10 18:34:15 +0200

    Created the picture endpoint

--- Server/app.py
+++ Server/app.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from flask import Response
+import base64
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Hello World!'
+@app.route('/picture')
+def index(picture):
+    if picture is not None:
+        # convert picture to base64
+        picture = picture.decode('utf-8')
+
+        return 'Hello World!'
 
 if __name__ == '__main__':
     app.run(debug=True)
