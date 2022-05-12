48b67bfd5240b42c036d295084a04613107c3632
Thuso-Khoza <56699783+Thuso-Khoza@users.noreply.github.com>
2022-05-11 20:45:03 -0700

    :heavy_plus_sign: converter service dependence addition

--- Front-End/Image-Converter/src/app/app.module.ts
+++ Front-End/Image-Converter/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ConverterComponent } from './converter/converter.component';
 import { MaterialModule } from './material/material.module';
+import {ConverterService} from './shared/converter.service';
 
 @NgModule({
   declarations: [
@@ -16,9 +18,10 @@ import { MaterialModule } from './material/material.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ConverterService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
