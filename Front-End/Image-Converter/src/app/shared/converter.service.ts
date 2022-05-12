46315341ce49b7c76468ac95c542396c83fbce16
Thuso-Khoza <56699783+Thuso-Khoza@users.noreply.github.com>
2022-05-11 20:41:13 -0700

    :sparkles: converter service
    contains code for http communication to back end

---
+++ Front-End/Image-Converter/src/app/shared/converter.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClientModule, HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConverterService {
+
+  constructor(private httpclient: HttpClient) { }
+
+   // postImg sends request to back end to upload img
+   postImg(img: File){
+     /* for future use
+    var auth;
+    if(localStorage.getItem('rememberMe')=="true"){
+      auth=localStorage.getItem('token');
+    }else{
+      auth=sessionStorage.getItem('token');
+    }
+
+    
+    const httpOptions = {
+      headers: new HttpHeaders({
+        Authorization: 'Bearer '+auth
+      })
+    };*/
+    const formData: FormData = new FormData();
+    formData.append('Image', img, img.name);
+    console.log("form: "+formData);
+    return this.httpclient.post(
+      'http://localhost:5000/api/User/uploadProfileImage',
+      formData//,httpOptions
+    );
+  }
+}
