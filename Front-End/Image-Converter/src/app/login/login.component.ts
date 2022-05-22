4343cdf1a068acc0edd0f7e0849ed657754cf96f
Thuso-Khoza <56699783+Thuso-Khoza@users.noreply.github.com>
2022-05-22 15:10:32 -0700

    :construction: login functionality

--- Front-End/Image-Converter/src/app/login/login.component.ts
+++ Front-End/Image-Converter/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup,FormControl,Validators} from '@angular/forms';
+import {ConverterService} from './../shared/converter.service';
+import { Observable, Subscriber } from 'rxjs';
+import { Login } from '../classes/Login';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  hide = true;
+  _match!: boolean;
+  buttonLogin = "";
+
+  constructor(private loginService: ConverterService) { }
 
   ngOnInit(): void {
   }
 
+  form = new FormGroup({  
+    username: new FormControl('', Validators.required),  
+    password: new FormControl('', Validators.required),
+    // submit: new FormControl()
+  });
+
+  get username() {  
+    return this.form.get('username');  
+  } 
+  get password() {  
+    return this.form.get('password');  
+  } 
+
+  register()
+  {
+    //intentionally left blank
+  }
+  onSubmit()
+  {
+    let authDetails:Login = {
+      email : this.form.get('username')!.value,
+      password : this.form.get('password')!.value
+    } 
+
+    this.loginService.login(authDetails).subscribe(
+      responseData=>{
+        console.log(responseData);
+      });
+      }
+      
 }
