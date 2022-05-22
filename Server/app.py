e1e6d5e51def6cd5e06182953a282e14259a968e
Phadimane Lerefolo <u19081082@tuks.co.za>
2022-05-22 16:32:43 +0200

    returning the image from the folders

--- Server/app.py
+++ Server/app.py
@@ -13,11 +13,12 @@ def upload_image():
     picture = request.json['picture']
     if picture is not None:
         print("picture is not None")
+        imageReturned = "data:image/png;base64,"
         with open("images/image1.jpg", "rb") as img_file:
             b64picture = base64.b64encode(img_file.read())
         print("b64picture")
 
-    return jsonify({'image': str(picture)})
+    return jsonify({'image': str(imageReturned+ b64picture.decode('UTF-8'))})
 
     
 @app.route('/')
