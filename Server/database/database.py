4d9adbddd452ead9dd820b7442afd5808c25343d
Phadimane Lerefolo <u19081082@tuks.co.za>
2022-05-22 15:23:54 +0200

    removed unnecessary imports

--- Server/database/database.py
+++ Server/database/database.py
@@ -1,14 +1,9 @@
 import bcrypt
-import random
-from flask import request, json
 from dotenv import load_dotenv
 import psycopg2.extras
 import psycopg2
-from flask import Flask
-from database.send_email import Email
 import os
 import sys
-import functools
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
 load_dotenv()
