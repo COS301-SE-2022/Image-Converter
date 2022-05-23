b223e17944a73f873c30e7f6527e42594d41c1dc
Phadimane Lerefolo <u19081082@tuks.co.za>
2022-05-23 12:04:30 +0200

    redirected login to endpoint

--- Front-End/Image-Converter/src/app/login/login.component.ts
+++ Front-End/Image-Converter/src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import {FormGroup,FormControl,Validators} from '@angular/forms';
 import {ConverterService} from './../shared/converter.service';
 import { Observable, Subscriber } from 'rxjs';
 import { Login } from '../classes/Login';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -15,11 +16,11 @@ export class LoginComponent implements OnInit {
   _match!: boolean;
   buttonLogin = "";
 
-  constructor(private loginService: ConverterService) { }
+  constructor(private loginService: ConverterService, private _router: Router) { }
 
   ngOnInit(): void {
   }
-
+  response!:{result:string,token:string};
   form = new FormGroup({  
     username: new FormControl('', Validators.required),  
     password: new FormControl('', Validators.required),
@@ -46,7 +47,14 @@ export class LoginComponent implements OnInit {
 
     this.loginService.login(authDetails).subscribe(
       responseData=>{
-        console.log(responseData);
+        console.log(responseData.body.result);
+        this.response = JSON.parse(JSON.stringify(responseData));
+        // console.log(responseData.body.token);
+        if(responseData.body.result == "success"){
+          localStorage.setItem('token', responseData.body.token);
+          this._router.navigateByUrl('/dashboard');
+        }
+        // this._router.navigateByUrl('/dashboard');
       });
       }
       
--- Front-End/Image-Converter/src/app/register/register.component.ts
+++ Front-End/Image-Converter/src/app/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup,FormControl,Validators} from '@angular/forms';
+import { Register } from '../classes/Register';
+import { ConverterService } from '../shared/converter.service';
+
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private registerService: ConverterService) { }
 
   ngOnInit(): void {
   }
+  form = new FormGroup({  
+    name: new FormControl('', Validators.required),  
+    surname: new FormControl('', Validators.required),  
+    email: new FormControl('', Validators.required),  
+    password: new FormControl('', Validators.required),
+    // submit: new FormControl()
+  });
 
+  get name() {  
+    return this.form.get('name');  
+  } 
+  get password() {  
+    return this.form.get('password');  
+  } 
+  get surname() {  
+    return this.form.get('surname');  
+  } 
+  get email() {  
+    return this.form.get('email');  
+  } 
+
+  register()
+  {
+    //intentionally left blank
+  }
+  onSubmit()
+  {
+    let authDetails:Register = {
+      
+      name: this.form.get('name')!.value,
+      surname: this.form.get('surname')!.value,
+      email: this.form.get('email')!.value,
+      password: this.form.get('password')!.value
+      
+    } 
+
+    this.registerService.register(authDetails).subscribe(
+      responseData=>{
+        console.log(responseData);
+      });
+      }
 }
