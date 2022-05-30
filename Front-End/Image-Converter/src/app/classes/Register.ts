331a9c44af5fce78bdef72396c9b503ddbb29cfe
OmolemoMashigo <93662161+OmolemoMashigo@users.noreply.github.com>
2022-05-30 13:06:55 +0200

    register changes

--- Front-End/Image-Converter/src/app/classes/Register.ts
+++ Front-End/Image-Converter/src/app/classes/Register.ts
@@ -3,5 +3,6 @@ export class Register {
     surname!: string;
     email!: string;
     password!: string;
+    confirmPassword!: string;
   }
   
--- Front-End/Image-Converter/src/app/register/register.component.ts
+++ Front-End/Image-Converter/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup,FormControl,Validators} from '@angular/forms';
+import {FormGroup,FormControl,FormBuilder,Validators} from '@angular/forms';
 import { Register } from '../classes/Register';
+import { CustomValidationService } from '../services/custom-validation.service';
 import { ConverterService } from '../shared/converter.service';
 
 
@@ -11,7 +12,7 @@ import { ConverterService } from '../shared/converter.service';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor(private registerService: ConverterService) { }
+  constructor(private registerService: ConverterService, private customValidator: CustomValidationService) { }
 
   ngOnInit(): void {
   }
@@ -20,7 +21,10 @@ export class RegisterComponent implements OnInit {
     surname: new FormControl('', Validators.required),  
     email: new FormControl('', Validators.required),  
     password: new FormControl('', Validators.required),
+    confirmpassword: new FormControl('', Validators.required),
     // submit: new FormControl()
+  },{
+    // validator: this.customValidator.passwordMatchValidator("password","confirmpassword")
   });
 
   get name() {  
@@ -28,6 +32,9 @@ export class RegisterComponent implements OnInit {
   } 
   get password() {  
     return this.form.get('password');  
+  } 
+  get confirmPassword() {  
+    return this.form.get('confirmPassword');  
   } 
   get surname() {  
     return this.form.get('surname');  
@@ -43,12 +50,11 @@ export class RegisterComponent implements OnInit {
   onSubmit()
   {
     let authDetails:Register = {
-      
       name: this.form.get('name')!.value,
       surname: this.form.get('surname')!.value,
       email: this.form.get('email')!.value,
-      password: this.form.get('password')!.value
-      
+      password: this.form.get('password')!.value,
+      confirmPassword: this.form.get('confirmPassword')!.value,
     } 
 
     this.registerService.register(authDetails).subscribe(
