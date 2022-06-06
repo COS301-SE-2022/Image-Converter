739e449cb4b97b951692105a50197fa0a31ff352
Thuso-Khoza <56699783+Thuso-Khoza@users.noreply.github.com>
2022-06-05 21:13:39 -0700

    :sparkles: creation of popup component

---
+++ Front-End/Image-Converter/src/app/image-popup/image-popup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-popup',
+  templateUrl: './image-popup.component.html',
+  styleUrls: ['./image-popup.component.scss']
+})
+export class ImagePopupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
