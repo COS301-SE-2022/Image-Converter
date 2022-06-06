baa6761963e9a658add587fed7b1b7469387fff6
Thuso-Khoza <56699783+Thuso-Khoza@users.noreply.github.com>
2022-06-05 22:00:56 -0700

    :package: addition of packages

--- Front-End/Image-Converter/src/app/app-routing.module.ts
+++ Front-End/Image-Converter/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import {ConverterComponent} from './converter/converter.component'
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { UploadHistoryComponent } from './upload-history/upload-history.component';
 
 const routes: Routes = [
   {
@@ -17,6 +18,10 @@ const routes: Routes = [
   {
     path: 'register',
     component: RegisterComponent
+  },
+  {
+    path: 'uploadHistory',
+    component: UploadHistoryComponent
   }
 
 ];
