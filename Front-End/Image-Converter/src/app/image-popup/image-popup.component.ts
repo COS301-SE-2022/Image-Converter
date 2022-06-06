889981283d8565e4e6f0ebe0fa016d27c60f6f95
Thuso-Khoza <56699783+Thuso-Khoza@users.noreply.github.com>
2022-06-05 21:56:31 -0700

    :construction: implementation of image popup functionality

--- Front-End/Image-Converter/src/app/image-popup/image-popup.component.ts
+++ Front-End/Image-Converter/src/app/image-popup/image-popup.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef,MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-image-popup',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImagePopupComponent implements OnInit {
 
-  constructor() { }
+  //holds the url of the image passed into this component
+  imageUrl!: string;
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: {img: string}) { }
 
   ngOnInit(): void {
+    this.imageUrl=this.data.img;
+    console.log(this.imageUrl);
   }
-
+ 
+  downloadFile() {
+    var a = document.createElement('a');
+    a.href = this.imageUrl;
+    var imgBckend = a.href;
+    a.download = "output.png";
+     document.body.appendChild(a);
+     a.click();
+     document.body.removeChild(a);
+  }
 }
