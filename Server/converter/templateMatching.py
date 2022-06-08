af47ca2d1b6e6b39cbd15c973fbc8f74851fc15f
Phadimane Lerefolo <u19081082@tuks.co.za>
2022-06-08 22:49:52 +0200

    cleaned and removed unnecessary comments

--- Server/converter/templateMatching.py
+++ Server/converter/templateMatching.py
@@ -4,30 +4,11 @@ import matplotlib.pyplot as plt
 
 class Matching:
     def __init__(self,uploaded_image):
-        # read image
-        # self.img = cv2.imread('draw.jpeg')
-        # cv.imshow('user_image',uploaded_image)
-        # cv.waitKey(0)
-        # cv.destroyAllWindows()
         self.uploaded_image = uploaded_image
         self.perfectMatch = 0
         self.graphType = ""
         self.match()
-        # graphType = ""
-    # Line Graph
-    # uploaded_image = cv.imread('assets/line-graph.jpg',cv.IMREAD_GRAYSCALE)          # queryImage
-    # uploaded_image = cv.imread('assets/line-graph2.webp',cv.IMREAD_GRAYSCALE)          # queryImage
 
-    # Bar Graph
-    # uploaded_image = cv.imread('assets/bargraph.png',cv.IMREAD_GRAYSCALE)          # queryImage
-    # uploaded_image = cv.imread('assets/barchart.jpg',cv.IMREAD_GRAYSCALE)          # queryImage
-
-    # Pie Chart
-    # uploaded_image = cv.imread('assets/piechart1.jpg',cv.IMREAD_GRAYSCALE)          # queryImage
-
-
-    # Scatterplot
-    # uploaded_image = cv.imread('../assets/scatterplot1.webp',cv.IMREAD_GRAYSCALE)          # queryImage
 
     def match(self):
         print(self.line_graph())
@@ -63,11 +44,6 @@ class Matching:
             self.perfectMatch = goodMatches
             self.graphType = "Line Graph"
         return goodMatches
-        # if(goodMatches > 5 ):
-        #     # graphType="linegraph"
-        #     # print("Graph is a "+ graphType)
-        #     return 
-
 
     def barchart(self):
         img2 = cv.imread('assets/barchart-template.jpg',cv.IMREAD_GRAYSCALE) 
@@ -97,7 +73,6 @@ class Matching:
             self.perfectMatch = goodMatches
             self.graphType = "Bar Graph"
         return goodMatches
-
 
 
     def pie_chart(self):
@@ -148,7 +123,6 @@ class Matching:
 
         matchesMask = [[0,0] for i in range(len(matches))]
 
-
         for i,(m,n) in enumerate(matches):
             if m.distance < 0.7*n.distance:
                 goodMatches+=1 
@@ -158,16 +132,3 @@ class Matching:
             self.perfectMatch = goodMatches
             self.graphType = "Scatterplot"
         return goodMatches
-
-
-
-
-# if __name__ == "__main__": 
-#     user_image = cv.imread('../assets/scatterplot1.webp',cv.IMREAD_GRAYSCALE)          # queryImage
-#     # cv.imshow('user_image',user_image)
-#     # cv.waitKey(0)
-#     # cv.destroyAllWindows()
-#     tempMatch = Matching(user_image)
-#     tempMatch.match()
-#     print(tempMatch.perfectMatch)
-#     print(tempMatch.graphType)
