0dab3129bb34bf9415842fd3db7f3a16f50ef817
MoeMoola3 <73942516+MoeMoola3@users.noreply.github.com>
2022-06-08 21:22:55 +0200

    🚧(py): Updated Template Matching Files

--- Server/template-matching.py
+++ Server/template-matching.py
@@ -12,176 +12,135 @@ graphType = ""
 # img1 = cv.imread('assets/barchart.jpg',cv.IMREAD_GRAYSCALE)          # queryImage
 
 # Pie Chart
-img1 = cv.imread('assets/piechart1.jpg',cv.IMREAD_GRAYSCALE)          # queryImage
+# img1 = cv.imread('assets/piechart1.jpg',cv.IMREAD_GRAYSCALE)          # queryImage
 
-# Parabola
-# img1 = cv.imread('assets/parabola1.jpg',cv.IMREAD_GRAYSCALE)          # queryImage
+
+# Scatterplot
+img1 = cv.imread('assets/scatterplot1.webp',cv.IMREAD_GRAYSCALE)          # queryImage
+
 
 
 def line_graph():
-    # img1 = cv.imread('assets/line-graph-copy.jpg',cv.IMREAD_GRAYSCALE)          # queryImage
-    img2 = cv.imread('assets/line-graph-copy.jpg',cv.IMREAD_GRAYSCALE) # trainImage
-    # # Initiate SIFT detector
-    sift = cv.SIFT_create()
-    # find the keypoints and descriptors with SIFT
-    kp1, des1 = sift.detectAndCompute(img1,None)
-    kp2, des2 = sift.detectAndCompute(img2,None)
+    img2 = cv.imread('assets/line-graph-copy.jpg',cv.IMREAD_GRAYSCALE)
 
-    # FLANN parameters
+    sift = cv.SIFT_create()
+    keypts1, desc1 = sift.detectAndCompute(img1,None)
+    keypts2, desc2 = sift.detectAndCompute(img2,None)
+
+   
     FLANN_INDEX_KDTREE = 1
     index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-    search_params = dict(checks=50)   # or pass empty dictionary
+    search_params = dict(checks=50)   
     flann = cv.FlannBasedMatcher(index_params,search_params)
-    matches = flann.knnMatch(des1,des2,k=2)
+    matches = flann.knnMatch(desc1,desc2,k=2)
 
     goodMatches = 0
-    # Need to draw only good matches, so create a mask
+
     matchesMask = [[0,0] for i in range(len(matches))]
 
 
-    # ratio test as per Lowe's paper
     for i,(m,n) in enumerate(matches):
         if m.distance < 0.7*n.distance:
             goodMatches+=1 
             matchesMask[i]=[1,0]
-    draw_params = dict(matchColor = (0,255,0),
-                    singlePointColor = (255,0,0),
-                    matchesMask = matchesMask,
-                    flags = cv.DrawMatchesFlags_DEFAULT)
-    img3 = cv.drawMatchesKnn(img1,kp1,img2,kp2,matches,None,**draw_params)
-    # print("Linegraph")
-    # print("Number of Matches: ",goodMatches)
-    # print()
+
 
     if(goodMatches > 5 ):
         graphType="linegraph"
         print("Graph is a "+ graphType)
 
-    # plt.imshow(img3),plt.show()
 
 def barchart():
-    # img1 = cv.imread('assets/bargraph.png',cv.IMREAD_GRAYSCALE)          # queryImage
-    img2 = cv.imread('assets/barchart-template.jpg',cv.IMREAD_GRAYSCALE) # trainImage
-    # # Initiate SIFT detector
-    sift = cv.SIFT_create()
-    # find the keypoints and descriptors with SIFT
-    kp1, des1 = sift.detectAndCompute(img1,None)
-    kp2, des2 = sift.detectAndCompute(img2,None)
+    img2 = cv.imread('assets/barchart-template.jpg',cv.IMREAD_GRAYSCALE) 
 
-    # FLANN parameters
+    sift = cv.SIFT_create()
+
+    keypts1, desc1 = sift.detectAndCompute(img1,None)
+    keypts2, desc2 = sift.detectAndCompute(img2,None)
+
+
     FLANN_INDEX_KDTREE = 1
     index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-    search_params = dict(checks=50)   # or pass empty dictionary
+    search_params = dict(checks=50)   
     flann = cv.FlannBasedMatcher(index_params,search_params)
-    matches = flann.knnMatch(des1,des2,k=2)
+    matches = flann.knnMatch(desc1,desc2,k=2)
 
     goodMatches = 0
-    # Need to draw only good matches, so create a mask
     matchesMask = [[0,0] for i in range(len(matches))]
 
 
-    # ratio test as per Lowe's paper
     for i,(m,n) in enumerate(matches):
         if m.distance < 0.7*n.distance:
             goodMatches+=1 
             matchesMask[i]=[1,0]
-    draw_params = dict(matchColor = (0,255,0),
-                    singlePointColor = (255,0,0),
-                    matchesMask = matchesMask,
-                    flags = cv.DrawMatchesFlags_DEFAULT)
-    img3 = cv.drawMatchesKnn(img1,kp1,img2,kp2,matches,None,**draw_params)
-    # print("Barchart")
-    # print("Number of Matches: ",goodMatches)
-    # print()
+
 
     if(goodMatches > 5 ):
         graphType = "barchart"
         print("Graph is a "+ graphType)
 
-    # plt.imshow(img3),plt.show()
 
 
 def pie_chart():
-    img2 = cv.imread('assets/piechart-template.jpg',cv.IMREAD_GRAYSCALE) # trainImage
-    # # Initiate SIFT detector
-    sift = cv.SIFT_create()
-    # find the keypoints and descriptors with SIFT
-    kp1, des1 = sift.detectAndCompute(img1,None)
-    kp2, des2 = sift.detectAndCompute(img2,None)
+    img2 = cv.imread('assets/piechart-template.jpg',cv.IMREAD_GRAYSCALE) 
 
-    # FLANN parameters
+    sift = cv.SIFT_create()
+
+    keypts1, desc1 = sift.detectAndCompute(img1,None)
+    keypts2, desc2 = sift.detectAndCompute(img2,None)
+
     FLANN_INDEX_KDTREE = 1
     index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-    search_params = dict(checks=50)   # or pass empty dictionary
+    search_params = dict(checks=50)   
     flann = cv.FlannBasedMatcher(index_params,search_params)
-    matches = flann.knnMatch(des1,des2,k=2)
+    matches = flann.knnMatch(desc1,desc2,k=2)
 
     goodMatches = 0
-    # Need to draw only good matches, so create a mask
+
     matchesMask = [[0,0] for i in range(len(matches))]
 
 
-    # ratio test as per Lowe's paper
     for i,(m,n) in enumerate(matches):
         if m.distance < 0.7*n.distance:
             goodMatches+=1 
             matchesMask[i]=[1,0]
-    draw_params = dict(matchColor = (0,255,0),
-                    singlePointColor = (255,0,0),
-                    matchesMask = matchesMask,
-                    flags = cv.DrawMatchesFlags_DEFAULT)
-    img3 = cv.drawMatchesKnn(img1,kp1,img2,kp2,matches,None,**draw_params)
-    # print("Piechart")
-    # print("Number of Matches: ",goodMatches)
-    # print()
+
+ 
 
     if(goodMatches > 5 ):
         graphType = "piechart"
         print("Graph is a "+ graphType)
 
 
-    # plt.imshow(img3),plt.show()
+def scatterplot():
+    img2 = cv.imread('assets/scatterplot-template.png',cv.IMREAD_GRAYSCALE) 
 
-def parabola():
-    img2 = cv.imread('assets/parabola-template-resized.png',cv.IMREAD_GRAYSCALE) # trainImage
-    # # Initiate SIFT detector
     sift = cv.SIFT_create()
-    # find the keypoints and descriptors with SIFT
-    kp1, des1 = sift.detectAndCompute(img1,None)
-    kp2, des2 = sift.detectAndCompute(img2,None)
 
-    # FLANN parameters
+    keypts1, desc1 = sift.detectAndCompute(img1,None)
+    keypts2, desc2 = sift.detectAndCompute(img2,None)
+
     FLANN_INDEX_KDTREE = 1
     index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
     search_params = dict(checks=50)   # or pass empty dictionary
     flann = cv.FlannBasedMatcher(index_params,search_params)
-    matches = flann.knnMatch(des1,des2,k=2)
+    matches = flann.knnMatch(desc1,desc2,k=2)
 
     goodMatches = 0
-    # Need to draw only good matches, so create a mask
+
     matchesMask = [[0,0] for i in range(len(matches))]
 
 
-    # ratio test as per Lowe's paper
     for i,(m,n) in enumerate(matches):
         if m.distance < 0.7*n.distance:
             goodMatches+=1 
             matchesMask[i]=[1,0]
-    draw_params = dict(matchColor = (0,255,0),
-                    singlePointColor = (255,0,0),
-                    matchesMask = matchesMask,
-                    flags = cv.DrawMatchesFlags_DEFAULT)
-    img3 = cv.drawMatchesKnn(img1,kp1,img2,kp2,matches,None,**draw_params)
-    # print("Parabola")
-    # print("Number of Matches: ",goodMatches)
-    # print()
+
 
     if(goodMatches > 5 ):
-        graphType = "parabola"
+        graphType = "scatterplot"
         print("Graph is a "+ graphType)
 
-
-    # plt.imshow(img3),plt.show()
 
 
 
@@ -190,5 +149,4 @@ if __name__ == "__main__":
     line_graph()
     barchart()
     pie_chart()
-    parabola()
-    # print(graphType)
+    scatterplot()
