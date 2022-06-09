5477f90f40e3a49611c5171ba76a55042905a90e
Thuso-Khoza <56699783+Thuso-Khoza@users.noreply.github.com>
2022-06-09 00:51:42 -0700

    :construction: delete functionality

--- Front-End/Image-Converter/src/app/image-popup/image-popup.component.ts
+++ Front-End/Image-Converter/src/app/image-popup/image-popup.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef,MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { UploadHistoryComponent } from '../upload-history/upload-history.component';
 
 @Component({
   selector: 'app-image-popup',
@@ -10,21 +11,27 @@ export class ImagePopupComponent implements OnInit {
 
   //holds the url of the image passed into this component
   imageUrl!: string;
-
-  constructor(@Inject(MAT_DIALOG_DATA) public data: {img: string}) { }
-
+  imageProcessedUrl!: string;
+  constructor(@Inject(MAT_DIALOG_DATA) public data: {img: string,imgProcessed:string},public dialogRef: MatDialogRef<UploadHistoryComponent>) { }
+  
   ngOnInit(): void {
     this.imageUrl=this.data.img;
+    this.imageProcessedUrl=this.data.imgProcessed;
     console.log(this.imageUrl);
   }
  
-  downloadFile() {
+  downloadFile(imageDownload:any) {
     var a = document.createElement('a');
-    a.href = this.imageUrl;
-    var imgBckend = a.href;
+    a.href = imageDownload;
+
     a.download = "output.png";
      document.body.appendChild(a);
      a.click();
      document.body.removeChild(a);
+  }
+
+  deleteImages()
+  {
+    this.dialogRef.close({request:"delete"});
   }
 }
