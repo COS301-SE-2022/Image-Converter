fd7487429ab9eda42c98d83e0c6d39c3ef86fdd2
Thuso-Khoza <56699783+Thuso-Khoza@users.noreply.github.com>
2022-06-09 13:59:59 -0700

    :hammer: contact functionality implementation

--- Front-End/Image-Converter/src/app/contact/contact.component.ts
+++ Front-End/Image-Converter/src/app/contact/contact.component.ts
@@ -1,4 +1,7 @@
+import { Message } from './../classes/Message';
 import { Component, OnInit } from '@angular/core';
+import {FormGroup,FormControl,Validators} from '@angular/forms';
+import {ConverterService} from './../shared/converter.service';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  constructor(private sendMessageService: ConverterService) { }
 
   ngOnInit(): void {
-  }
 
+    this.form.patchValue({
+      email: localStorage.getItem('email')
+    });
+    
+  }
+  
+  //used for loadig spinner
+  loading=false;
+
+  form = new FormGroup({  
+    email: new FormControl('', [Validators.required, Validators.email]),  
+    message: new FormControl('', Validators.required),
+  });
+
+  onSubmit(){
+      this.loading = true;
+
+      let messageDetails:Message = {
+        email : this.form.get('email')!.value,
+        message : this.form.get('message')!.value
+      } 
+
+      this.sendMessageService.sendMessage(messageDetails).subscribe(
+        responseData=>{
+          this.loading = true;
+
+          /*this.response = JSON.parse(JSON.stringify(responseData));
+          // console.log(responseData.body.token);
+          if(responseData.body.result == "success"){
+            
+          }*/
+          //if success
+          alert("Message successfully sent")
+        });
+
+  }
 }
