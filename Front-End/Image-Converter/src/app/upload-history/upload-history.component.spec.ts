52efc55cde26da5ee542b788ab422aace113bdb6
Thuso-Khoza <56699783+Thuso-Khoza@users.noreply.github.com>
2022-06-21 19:23:14 -0700

    :white_check_mark: upload history service stub

--- Front-End/Image-Converter/src/app/upload-history/upload-history.component.spec.ts
+++ Front-End/Image-Converter/src/app/upload-history/upload-history.component.spec.ts
@@ -1,14 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UploadHistoryComponent } from './upload-history.component';
+import {ConverterService} from './../shared/converter.service';
+
 
 describe('UploadHistoryComponent', () => {
   let component: UploadHistoryComponent;
   let fixture: ComponentFixture<UploadHistoryComponent>;
 
+    //used in the stead of real service
+  let serviceStub:any;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UploadHistoryComponent ]
+      declarations: [ UploadHistoryComponent ],
+      providers: [ { provide: ConverterService, useValue: serviceStub } ]
     })
     .compileComponents();
   });
