15750e68fed1b5771c4b888ba64f46b073ed81c1
Thuso-Khoza <56699783+Thuso-Khoza@users.noreply.github.com>
2022-06-21 19:20:29 -0700

    :white_check_mark: contact component test

--- Front-End/Image-Converter/src/app/contact/contact.component.spec.ts
+++ Front-End/Image-Converter/src/app/contact/contact.component.spec.ts
@@ -1,14 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs';
 import { ContactComponent } from './contact.component';
+import {ConverterService} from './../shared/converter.service';
 
 describe('ContactComponent', () => {
   let component: ContactComponent;
   let fixture: ComponentFixture<ContactComponent>;
 
+  //used in the stead of real service
+  let serviceStub:any;
+
   beforeEach(async () => {
+
+    serviceStub={
+      sendMessage:()=> of('m')
+    };
     await TestBed.configureTestingModule({
-      declarations: [ ContactComponent ]
+      declarations: [ ContactComponent ],
+      providers: [ { provide: ConverterService, useValue: serviceStub } ]
     })
     .compileComponents();
   });
@@ -19,7 +28,18 @@ describe('ContactComponent', () => {
     fixture.detectChanges();
   });
 
+  var a;
+
+  it("and so is a spec", function() {
+    a = true;
+
+    expect(a).toBe(true);
+  });
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show loading variable as false (should not be showing loading icon)', () => {
+    expect(component.loading).toBeFalsy();
   });
 });
