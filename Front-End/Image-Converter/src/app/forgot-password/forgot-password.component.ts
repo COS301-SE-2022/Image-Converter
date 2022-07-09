5662e366a6085ef6d6ad406554f781fcd3adb8a0
Thuso-Khoza <56699783+Thuso-Khoza@users.noreply.github.com>
2022-07-08 20:21:39 -0700

    :construction: forgot password functionality

--- Front-End/Image-Converter/src/app/forgot-password/forgot-password.component.ts
+++ Front-End/Image-Converter/src/app/forgot-password/forgot-password.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {ConverterService} from './../shared/converter.service';
 
 @Component({
   selector: 'app-forgot-password',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotPasswordComponent implements OnInit {
 
-  constructor() { }
+  constructor(private resetService: ConverterService) { }
 
   ngOnInit(): void {
   }
 
+  _match!: boolean;
+  buttonReset = "";
+
+  form = new FormGroup({  
+    email: new FormControl('', Validators.required)
+  });
+
+  formCode = new FormGroup({  
+    code: new FormControl('', Validators.required)
+  });
+
+  onSubmit(){
+    console.log("clicked");
+
+    //once code is sent through and response is given
+    document.getElementById("resetForm")!.style.display="none";
+    document.getElementById("codeForm")!.style.display="inline-block";
+
+   /* this.resetService.ResetPassword(this.form.get('email')!.value).subscribe(
+      responseData =>{
+            //response
+        }
+    );*/
+  }
+
+  onSubmitCode()
+  {
+    console.log("clicked code");
+    /* this.resetService.ResetPasswordCode(this.form.get('code')!.value).subscribe(
+      responseData =>{
+            //response
+        }
+    );*/
+  }
 }
