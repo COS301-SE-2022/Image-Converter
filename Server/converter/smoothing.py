58026a22c7e9aab8b80257e2fefbe747ea3a4db9
Neo Seefane <100659373+neoseefane@users.noreply.github.com>
2022-07-19 16:52:45 +0200

    ✨ (Back-end) Image resizing

--- Server/converter/smoothing.py
+++ Server/converter/smoothing.py
@@ -8,7 +8,7 @@ class smoothing:
 
     def clean_noise(self):
         # blur
-        blur = cv2.bilateralFilter(img[i].copy(), 9, 75, 75)
+        blur = cv2.bilateralFilter(self.img, 9, 75, 75)
 
         # convert to hsv and get saturation channel
         sat = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)[:,:,1]
@@ -34,60 +34,65 @@ class smoothing:
         return image
 
 if __name__ == '__main__':
-    images = ['download.png', 'draw.jpeg', 'graph.jpeg', 'charts.png', 'google.jpeg', 'barGraph.jpeg']
-    img = []
-    saturation = []
-    bilateralBlur = []
-    threshold = []
-    # print(img)
+    # images = ['barGraph.jpeg', 'balloons_noisy.png']
+    # img = []
+    # saturation = []
+    # bilateralBlur = []
+    # threshold = []
+    
+    # # print(img)
 
-    for i in range(len(images)):
-        img.append(cv2.imread(images[i]))
+    # for i in range(len(images)):
+    #     img.append(cv2.imread(images[i]))
 
-    #Applying the blurring algorithms
-    for i in range(len(img)):
-        bilateralBlur.append(cv2.bilateralFilter(img[i].copy(), 15, 75, 75))
+    # #Applying the blurring algorithms
+    # nums = [9, 15, 18, 21, 24, 27, 30]
 
-    kernel = np.array([[-1, -1, -1],
-                       [-1, 9, -1],
-                       [-1, -1, -1]])
-    filter = np.array([[0, 0, -1, 0, 0], [0, -1, -2, -1, 0], [-1, -2, 16, -2, -1], [0, -1, -2, -1, 0], [0, 0, -1, 0, 0]])
-    for i in range(len(img)):
-        saturation.append(cv2.filter2D(src=bilateralBlur[i], ddepth=-1, kernel=filter))
-        # threshold.append(cv2.filter2D(src=img[i].copy(), ddepth=-1, kernel=kernel))
-        # bilateralBlur[i] = cv2.filter2D(img[i].copy(), -1, filter)
+    # for i in range(len(img)):
+    #     bilateralBlur.append(cv2.bilateralFilter(img[i].copy(), 15, 75, 75))
+
+    # kernel = np.array([[-1, -1, -1],
+    #                    [-1, 9, -1],
+    #                    [-1, -1, -1]])
+    # for i in range(len(img)):
+    #     saturation.append(cv2.filter2D(src=bilateralBlur[i], ddepth=-1, kernel=kernel))
+    #     # threshold.append(cv2.filter2D(src=img[i].copy(), ddepth=-1, kernel=kernel))
+    #     # bilateralBlur[i] = cv2.filter2D(img[i].copy(), -1, filter)
+
+    
+    # #apply morphology close and open
+    # input = [3, 4, 5, 6, 9, 12, 15, 18]
+    # close = []
+    # open = []
+    # th1 = []
+    # th2 = []
+    # th3 = []
+    # th4 = []
+    # th5 = []
+    # img = img.img_to_array(img, dtype='uint8')
+
+    # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
+    # for i in range(len(bilateralBlur)): 
+    #     th1.append(cv2.adaptiveThreshold(img[i], 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 21, 4))
+    #     close.append(cv2.morphologyEx(img[i], cv2.MORPH_CLOSE, kernel, iterations=1))
+    #     open.append(cv2.morphologyEx(th3[i], cv2.MORPH_OPEN, kernel, iterations=1))
+    src = 'barGraph.jpeg'
+    img = cv2.imread(src)
+    blurred = cv2.bilateralFilter(img, 15, 75, 75)
+    adapt = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 21, 10)
 
 
-    # convert to hsv and get saturation channel
-    threshold.clear()
-    for i in range(len(saturation)):
-        threshold.append(cv2.cvtColor(saturation[i], cv2.COLOR_BGR2HSV))
-
-    # threshold saturation channel
-    # for i in range(len(saturation)):
-    #     threshold[i] = cv2.threshold(threshold[i], 50, 255, cv2.THRESH_BINARY)[1]
-
-    # apply morphology close and open to make mask
-    # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (9, 9))
-    # morph = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=1)
-    # mask = cv2.morphologyEx(morph, cv2.MORPH_OPEN, kernel, iterations=1)
-    # morph = []
-    # mask = []
-    # for i in range(len(saturation)):
-    #     mask.append(cv2.Canny(saturation[i], 100, 200))
-        # obj = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (9, 9))
-        # morph.append(cv2.morphologyEx(saturation[i], cv2.MORPH_CLOSE, obj, iterations=1))
-        # mask.append(cv2.morphologyEx(morph[i], cv2.MORPH_OPEN, obj, iterations=1))
-
-
-    for i in range(len(img)):
-        cv2.imshow("Original_Image", img[i])
-        # cv2.imshow("MexicanHatExlBlur", bilateralBlur[i])
-        cv2.imshow("MexicanHatBlur", saturation[i])
-        # cv2.imshow("MexicanToHsv", threshold[i])
-        # cv2.imshow("Morphology", morph[i])
-        # cv2.imshow("Edges", mask[i])
-        cv2.waitKey(0)
-
+    # for i in range(len(close)):
+    #     cv2.imshow("Original_Image", img[i])
+    #     # cv2.imshow("Close " + str(i), close[i])
+    #     cv2.imshow("Open", open[i])
+       
+    #     cv2.imshow("th1", th1[i])
+    #     cv2.imshow("th2", th2[i])
+    #     cv2.imshow("th3", th3[i])
+    #     cv2.imshow("th4", th4[i])
+    #     cv2.imshow("th5", th5[i])
+    #     cv2.waitKey(0)
+    cv2.imshow("Original_Image", src)
+    cv2.imshow("Adaptive", adapt)
     cv2.destroyAllWindows()
-
