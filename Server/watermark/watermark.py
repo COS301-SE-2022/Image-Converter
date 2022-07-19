3b9ce1678be4af79556292e473319823215cbe87
OmolemoMashigo <93662161+OmolemoMashigo@users.noreply.github.com>
2022-07-19 15:55:25 +0200

    watermark image added

--- Server/watermark/watermark.py
+++ Server/watermark/watermark.py
@@ -1,9 +1,17 @@
-from PIL import Image, ImageDraw, ImageFont
-from tkinter import filedialog
-from tkinter import Tk
-import os
+# from PIL import Image, ImageDraw, ImageFont
+# from tkinter import filedialog
+# from tkinter import Tk
+# import os
 
 
-root = Tk()
-root.withdraw()
-filename = filedialog.askopenfilename(initialdir='C:\\Users\\Mama Modiselle\\Pictures\\example', title='Select Image')
+# root = Tk()
+# root.withdraw()
+# filename = filedialog.askopenfilename(initialdir='C:\\Users\\Mama Modiselle\\Pictures\\example', title='Select Image')
+
+import cv2
+import numpy as np
+
+logo = cv2.imread("watermark.jpg")
+
+cv2.imshow("logo", logo)
+cv2.waitKey(0)
