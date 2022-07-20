9641ba2bf56b8fb9c7e431771d456a10aa6032f3
OmolemoMashigo <93662161+OmolemoMashigo@users.noreply.github.com>
2022-07-20 15:29:34 +0200

    Update watermark.py

--- Server/watermark/watermark.py
+++ Server/watermark/watermark.py
@@ -1,37 +1,39 @@
-# from PIL import Image, ImageDraw, ImageFont
-# from tkinter import filedialog
-# from tkinter import Tk
-# import os
-
-
-# root = Tk()
-# root.withdraw()
-# filename = filedialog.askopenfilename(initialdir='C:\\Users\\Mama Modiselle\\Pictures\\example', title='Select Image')
-
 import cv2
 import numpy as np
+import glob
+import os
 
-mark = cv2.imread("watermark.jpg")
-h_mark, w_mark, _ = mark.shape
+logo = cv2.imread("logo/logo-test.png")
+# print(logo)
+h_logo, w_logo, _ = logo.shape
+
+images_path = glob.glob("images/*.*")
+
+print("Adding watermark")
+for img_path in images_path:
+    img = cv2.imread(img_path)
+    h_img, w_img, _ = img.shape
+
+    # Get the center of the original. It's the location where we will place the watermark
+    center_y = int(h_img / 2)
+    center_x = int(w_img / 2)
+    top_y = center_y - int(h_logo / 20)
+    left_x = center_x - int(w_logo / 15)
+    bottom_y = top_y + h_logo
+    right_x = left_x + w_logo
+
+    # Get ROI
+    roi = img[top_y: bottom_y, left_x: right_x]
+
+    # Add the Logo to the Roi
+    result = cv2.addWeighted(roi, 1, logo, 0.03, 0)
+
+    # Replace the ROI on the image
+    img[top_y: bottom_y, left_x: right_x] = result
+
+    # Get filename and save the image
+    filename = os.path.basename(img_path)
+    cv2.imwrite("images/watermarked_" + filename, img)
 
 
-img = cv2.imread('assets/barchart.jpg')
-h_img, w_img, _ = img.shape
-
-print(h_img, w_img)
-
-centre_y = int(h_img / 2)
-centre_x = int(w_img / 2)
-top_y = centre_y - (h_mark / 2)
-left_x = centre_x - int(w_mark / 2)
-bottom_y = top_y + h_mark
-right_x = left_x + w_mark
-
-# cv2.circle(img, (left_x, top_y), 10, (0, 255, 0), -1)#comment out
-
-# region of interest
-img[top_y: bottom_y, left_x: right_x]
-
-cv2.imshow("Img", img)
-cv2.imshow("logo", mark)
-cv2.waitKey(0)
+print("Watermark added to all the images")
