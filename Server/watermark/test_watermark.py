6e7f825da68f24df919c84d159db0ee5e8470083
OmolemoMashigo <93662161+OmolemoMashigo@users.noreply.github.com>
2022-07-24 20:42:45 +0200

    test update

--- Server/watermark/test_watermark.py
+++ Server/watermark/test_watermark.py
@@ -3,37 +3,56 @@ import os
 from watermark import AddMark
 from PIL import Image
 
-# class TestWatermark(unittest.TestCase):
 
-def test_watermark():
-    testImg = Image.open('images/bar3.jpg')
-    testLogo = Image.open('logo/logo-test.png')
+class TestWatermark(unittest.TestCase):
+    def test_watermark(self):
+        if os.path.exists('images/bar3.jpg') and os.path.exists('logo/logo-test.png'):
 
-    if os.path.exists('images/bar3.jpg') and os.path.exists('logo/logo-test.png'):
+            obj = AddMark(Image.open('images/bar3.jpg'))
 
-        width, height = testImg.size
+            obj.Dev()
+        else:
+            print("file path doen not exist")
 
-        size = (100, 100)
+    def test_save_FinalImage(self):
+        obj2 = AddMark(Image.open('images/bar3.jpg'))
 
-        testLogo.thumbnail(size)
+        if os.path.exists('results'):
+            obj2.Dev()
 
-        
-        # Location where we want to paste watermark on the uploaded image
-        x = width - 100 - 10
-        y = height - 100 - 0
-
-        testImg.size.paste(testLogo, (x, y))
-
-        assert os.path.exists('images/bar3.jpg') == True
-        assert os.path.exists('logo/logo-test.png') == False
-
-    if os.path.exists('saveTestImg'):
-        testImg.save('saveTestImg/testImg_with_logo.jpg')
-        assert os.path.exists('saveTestImg') == True
-
-    else:
-        assert os.path.exists('saveTestImg') == False
+        else:
+            print("results folder does not exist")
 
 
-if __name__ == '__main__':
-    unittest.main()
+# def test_watermark():
+#     testImg = Image.open('images/bar3.jpg')
+#     testLogo = Image.open('logo/logo-test.png')
+
+#     if os.path.exists('images/bar3.jpg') and os.path.exists('logo/logo-test.png'):
+
+#         width, height = testImg.size
+
+#         size = (100, 100)
+
+#         testLogo.thumbnail(size)
+
+
+#         # Location where we want to paste watermark on the uploaded image
+#         x = width - 100 - 10
+#         y = height - 100 - 0
+
+#         testImg.size.paste(testLogo, (x, y))
+
+#         assert os.path.exists('images/bar3.jpg') == True
+#         assert os.path.exists('logo/logo-test.png') == False
+
+#     if os.path.exists('saveTestImg'):
+#         testImg.save('saveTestImg/testImg_with_logo.jpg')
+#         assert os.path.exists('saveTestImg') == True
+
+#     else:
+#         assert os.path.exists('saveTestImg') == False
+
+
+# if __name__ == '__main__':
+#     unittest.main()
