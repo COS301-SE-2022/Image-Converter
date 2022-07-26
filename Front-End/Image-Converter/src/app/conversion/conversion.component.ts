e64620116b189312d7c2fa805ff1664199075f9c
Thuso-Khoza <56699783+Thuso-Khoza@users.noreply.github.com>
2022-07-26 12:00:36 -0700

    :construction: image format functionality

--- Front-End/Image-Converter/src/app/conversion/conversion.component.ts
+++ Front-End/Image-Converter/src/app/conversion/conversion.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ComponentCommunicationService } from './../shared/component-communication.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-conversion',
@@ -7,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConversionComponent implements OnInit {
 
-  constructor() { }
+  //these variables are used for the communication between converter and filter components
+  message!: any;
+  message2!: any;
+  dispBool!: boolean;
+  subscription!: Subscription;
+
+  constructor(private imgData: ComponentCommunicationService) { }
 
   ngOnInit(): void {
+    //subscribe for communication between components
+    this.subscription = this.imgData.currentMessage.subscribe(message => this.message = message);
+    //this.subscription = this.imgData.currentMessage2.subscribe(message=>this.message2=message);
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+   //download png function
+  downloadPngFile()
+  {
+    var a = document.createElement('a');
+    a.href = this.message.png;
+    var imgBckend = a.href;
+    a.download = "output.png";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  }
+
+  //download jpg function
+  downloadJpgFile()
+  {
+    var a = document.createElement('a');
+    console.log("jpeg: "+this.message.jpg);
+    a.href = this.message.jpg;
+    var imgBckend = a.href;
+    a.download = "output.jpg";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  }
 }
