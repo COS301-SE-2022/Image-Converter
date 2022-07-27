4eb24bd6997ff8400daabe2d63c3a2e74edcaecd
Neo Seefane <100659373+neoseefane@users.noreply.github.com>
2022-07-27 21:51:03 +0200

    🧪 (Back-end) testing imageRezing.py

--- Server/test/test_image_resizing.py
+++ Server/test/test_image_resizing.py
@@ -1,19 +1,20 @@
-from image_resizing.resizing import imageResizing
-from PIL import Image
-
+from converter.resizing import imageResizing
+import cv2
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
 def testImageSizeBeforeResizing():
     #prepare
-    object = imageResizing(Image.open('draw.jpeg'))
-
+    object = imageResizing(cv2.imread('./../draw.jpeg'))
+    # print(object.resizedImage.shape)
     #act
-    assert object.resizedImage.size == (3104, 3027)
+    assert object.resizedImage.shape == (3027, 3104, 3)
 
 def testImageSizeBeforeAndAfterResizing():
     #prepare
-    object = imageResizing(Image.open('draw.jpeg'))
-
+    object = imageResizing(cv2.imread('./../draw.jpeg'))
     #act
-    assert object.resizedImage.size == (3104, 3027)
+    assert object.resizedImage.shape == (3027, 3104, 3)
     object.resize()
-    assert object.resizedImage.size == (800, 800)
+    assert object.resizedImage.shape == (800, 800, 3)
