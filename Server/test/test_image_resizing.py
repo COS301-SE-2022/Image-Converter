e01693c23cf890256fae82346e88436e160e1f3e
Neo Seefane <100659373+neoseefane@users.noreply.github.com>
2022-07-29 03:04:26 +0200

    🧪 (Back-end) testing image resizing trail no.1

--- Server/test/test_image_resizing.py
+++ Server/test/test_image_resizing.py
@@ -1,19 +1,26 @@
 from converter.resizing import imageResizing
 import cv2
-import os
-import sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+from pathlib import Path
+
 
 def testImageSizeBeforeResizing():
     #Prepare data
-    object = imageResizing(cv2.imread('./../draw.jpeg'))
-   
+    workingDir = Path(__file__).parent  
+    filePath = workingDir / 'draw.jpeg'  
+
+    #Act
+    object = imageResizing(cv2.imread(str(filePath)))
+
     #Assert
     assert object.resizedImage.shape == (3027, 3104, 3)
 
 def testImageSizeBeforeAndAfterResizing():
     #Prepare data
-    object = imageResizing(cv2.imread('./../draw.jpeg'))
+    workingDir = Path(__file__).parent  
+    filePath = workingDir / 'draw.jpeg' 
+
+    #Act
+    object = imageResizing(cv2.imread(str(filePath)))
     
     #Assert
     assert object.resizedImage.shape == (3027, 3104, 3)
