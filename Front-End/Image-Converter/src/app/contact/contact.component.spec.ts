8957b06017d4244ff68798c80992e4db264323d7
Thuso-Khoza <56699783+Thuso-Khoza@users.noreply.github.com>
2022-08-03 11:44:00 -0700

    testing

--- Front-End/Image-Converter/src/app/contact/contact.component.spec.ts
+++ Front-End/Image-Converter/src/app/contact/contact.component.spec.ts
@@ -1,14 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContactComponent } from './contact.component';
+import {ConverterService} from './../shared/converter.service';
+import { of } from 'rxjs';
 
 describe('ContactComponent', () => {
   let component: ContactComponent;
   let fixture: ComponentFixture<ContactComponent>;
+  let serviceStub: any;
 
   beforeEach(async () => {
+
+    serviceStub = {
+      getContent: () => of('You have been warned'),
+    };
+    
     await TestBed.configureTestingModule({
-      declarations: [ ContactComponent ]
+      declarations: [ ContactComponent ],
+      providers: [ { provide: ConverterService, useValue: serviceStub } ]
     })
     .compileComponents();
   });
@@ -21,5 +30,21 @@ describe('ContactComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have loading as false', () => {
+    expect(component.loading).toBeFalsy();
+  });
+
+  it('should have response saying Message successfully sent', () => {
+    expect(component.onSubmit).toBeTruthy();
+  });
+
+  it('should have invalid email', () => {
+    
+    let email= component.form.controls.email;
+ 
+    email.setValue('f')
+    expect(email.errors).toBeTruthy();
   });
 });
--- Front-End/Image-Converter/src/app/conversion/conversion.component.spec.ts
+++ Front-End/Image-Converter/src/app/conversion/conversion.component.spec.ts
@@ -1,12 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ConversionComponent } from './conversion.component';
+import { ComponentCommunicationService } from './../shared/component-communication.service';
+
+import { of } from 'rxjs';
 
 describe('ConversionComponent', () => {
   let component: ConversionComponent;
   let fixture: ComponentFixture<ConversionComponent>;
 
+  let serviceStub: any;
+
   beforeEach(async () => {
+
+    serviceStub={}
     await TestBed.configureTestingModule({
       declarations: [ ConversionComponent ]
     })
@@ -21,5 +28,25 @@ describe('ConversionComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+ /* it('should have message with "download png"', () => {
+    expect(component.content).toContain('warn');
+  });*/
+
+  
+  it('should have message with "default message" from ComponentCommunicationService', () => {
+    expect(component.message).toContain('default message');
+  });
+
+  it('should download png', () => {
+    expect(component.downloadPngFile).toBeTruthy();
+  });
+  it('should download jpg', () => {
+    expect(component.downloadJpgFile).toBeTruthy();
+  });
+
+  it('should have dispBool false from ComponentCommunicationService', () => {
+    expect(component.dispBool).toBeFalsy();;
   });
 });
