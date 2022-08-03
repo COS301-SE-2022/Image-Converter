fed4813f7f51883e669d41845cba15c944102c8e
Phadimane Lerefolo <u19081082@tuks.co.za>
2022-08-03 19:51:38 +0200

    Update dashboard.component.spec.ts

--- Front-End/Image-Converter/src/app/dashboard/dashboard.component.spec.ts
+++ Front-End/Image-Converter/src/app/dashboard/dashboard.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing'
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { DashboardComponent } from './dashboard.component';
+import { Router } from '@angular/router';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
@@ -8,7 +10,14 @@ describe('DashboardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [ DashboardComponent ],
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule, 
+      ],
+      providers: [
+        Router
+      ]
     })
     .compileComponents();
   });
@@ -19,7 +28,15 @@ describe('DashboardComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the dashboard component', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('testing logout method', async() => {
+    it('', () => {
+      component.logout();
+      // expect(component).toEqual('');
+    });
+  });
+
 });
