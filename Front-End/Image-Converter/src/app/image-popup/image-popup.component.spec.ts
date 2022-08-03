7391a9077a64623dad3d364576cade3659910409
Phadimane Lerefolo <u19081082@tuks.co.za>
2022-08-03 19:09:20 +0200

    Update image-popup.component.spec.ts

--- Front-End/Image-Converter/src/app/image-popup/image-popup.component.spec.ts
+++ Front-End/Image-Converter/src/app/image-popup/image-popup.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing'
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ImagePopupComponent } from './image-popup.component';
+import {UploadHistoryComponent } from '../upload-history/upload-history.component';
 
 describe('ImagePopupComponent', () => {
   let component: ImagePopupComponent;
@@ -8,7 +10,14 @@ describe('ImagePopupComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ImagePopupComponent ]
+      declarations: [ ImagePopupComponent ] ,
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule, 
+      ],
+      providers: [
+        UploadHistoryComponent
+      ]
     })
     .compileComponents();
   });
@@ -19,7 +28,26 @@ describe('ImagePopupComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create image pop up component', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('testing downloadFile method', async() => {
+    const blob = new Blob([""], { type: "text/html" });
+    const file = <File>blob;
+    it('', () => {
+      component.downloadFile(file);
+      // expect(component).toEqual('');
+    });
+  });
+
+  describe('testing deleteImages method', async() => {
+    const blob = new Blob([""], { type: "text/html" });
+    const file = <File>blob;
+    it('', () => {
+      component.deleteImages();
+      // expect(component).toEqual('');
+    });
+  });
+
 });
