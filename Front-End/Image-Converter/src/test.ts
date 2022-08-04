12aaa5b581cc2e95e5e81a8f36d461c4d125c2d2
OmolemoMashigo <93662161+OmolemoMashigo@users.noreply.github.com>
2022-08-04 13:47:13 +0200

    Update test.ts

--- Front-End/Image-Converter/src/test.ts
+++ Front-End/Image-Converter/src/test.ts
@@ -22,8 +22,6 @@ getTestBed().initTestEnvironment(
 
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
-// const context = require.context('./', true, /register.component.spec\.ts$/);
-// const context = require.context('./', true, /forgot-password.component.spec\.ts$/);
 
 // And load the modules.
 context.keys().map(context);
