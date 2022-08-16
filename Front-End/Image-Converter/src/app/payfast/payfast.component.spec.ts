2b7762ecc1818d0f36aae2c3d8cb23ecdd699645
OmolemoMashigo <93662161+OmolemoMashigo@users.noreply.github.com>
2022-08-16 23:11:15 +0200

    payment gateway

---
+++ Front-End/Image-Converter/src/app/payfast/payfast.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PayfastComponent } from './payfast.component';
+
+describe('PayfastComponent', () => {
+  let component: PayfastComponent;
+  let fixture: ComponentFixture<PayfastComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PayfastComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PayfastComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Front-End/Image-Converter/src/app/payfast/payfast.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payfast',
+  templateUrl: './payfast.component.html',
+  styleUrls: ['./payfast.component.scss']
+})
+export class PayfastComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
