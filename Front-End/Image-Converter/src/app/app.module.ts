50c67dc87254134eca8271145037159e6aced9dd
OmolemoMashigo <93662161+OmolemoMashigo@users.noreply.github.com>
2022-08-23 20:23:34 +0200

    desmos graphing system

--- Front-End/Image-Converter/src/app/app.module.ts
+++ Front-End/Image-Converter/src/app/app.module.ts
@@ -21,6 +21,7 @@ import { TemplateMatchingComponent } from './template-matching/template-matching
 import { ImagePopupComponent } from './image-popup/image-popup.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { GraphPlottingComponent } from './graph-plotting/graph-plotting.component';
+import { LiveGraphing2Component } from './live-graphing2/live-graphing2.component';
 
 @NgModule({
   declarations: [
@@ -37,7 +38,8 @@ import { GraphPlottingComponent } from './graph-plotting/graph-plotting.componen
     TemplateMatchingComponent,
     ImagePopupComponent,
     ForgotPasswordComponent,
-    GraphPlottingComponent
+    GraphPlottingComponent,
+    LiveGraphing2Component
   ],
   imports: [
     BrowserModule,
---
+++ Front-End/Image-Converter/src/app/live-graphing2/live-graphing2.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+// // import {Desmos} from 'desmos';
+// import {Chart} from 'chart.js';
+
+// declare var Desmos: any;
+
+@Component({
+  selector: 'app-live-graphing2',
+  templateUrl: './live-graphing2.component.html',
+  styleUrls: ['./live-graphing2.component.scss']
+})
+export class LiveGraphing2Component implements OnInit {
+  myScriptElement: HTMLScriptElement;
+  Desmos: any;
+
+  constructor() { 
+    this.myScriptElement = document.createElement("script");
+    this.myScriptElement.src = "https://www.desmos.com/api/v1.7/calculator.js?apiKey=dcb31709b452b1cf9dc26972add0fda6";
+    document.body.appendChild(this.myScriptElement);
+  }
+
+  ngOnInit(): void {
+
+      this.myScriptElement.onload = () => {
+        this.Desmos = (<any>window).Desmos;
+        console.log('loaded');
+        document.getElementsByTagName('head')[0].appendChild(this.myScriptElement);
+        var elt = document.getElementById('calculator');
+        // var calculator = this.Desmos.GraphingCalculator(elt, { keypad: true, expressions: true, settingsMenu: false, expressionsCollapsed: true });
+        var calculator = this.Desmos.GraphingCalculator(elt);
+
+      
+        calculator.setExpression({});
+        // calculator.setExpression({id: 'graph1', latex: 'y=x^2'});
+
+        // var img1x = document.getElementById('screenshot-1x');
+        
+    }
+
+  
+
+  }
+
+    // captureScreenshots () {
+    // img1x.src = calculator.screenshot({
+    //   height: 500,
+    //   width: 500
+    // });
+
+
+
+
+}
