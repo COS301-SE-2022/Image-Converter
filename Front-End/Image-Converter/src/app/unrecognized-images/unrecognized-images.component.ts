fdaf20228d1caafcb06b187745c09f0b60c2dcad
Phadimane Lerefolo <u19081082@tuks.co.za>
2022-08-23 18:31:07 +0200

    Displaying the unrecognizedimages

--- Front-End/Image-Converter/src/app/unrecognized-images/unrecognized-images.component.ts
+++ Front-End/Image-Converter/src/app/unrecognized-images/unrecognized-images.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ConverterService } from '../shared/converter.service';
 
 @Component({
   selector: 'app-unrecognized-images',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UnrecognizedImagesComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private imgService: ConverterService) { }
+    //holds unprocessed images
+    uploadedImg: string[] = [];
+    //holds unprocessed images
+    uploadedImgProcessed: string[] = [];
+  
+    uuid:BigInteger[]=[];
+    //used for loadig spinner
+    loading=false;
   ngOnInit(): void {
+    this.loading = true;
+    let respsonseBase64:any="";
+      this.imgService.getUnrecognizedGraphs().subscribe(
+        responseData =>{
+          this.loading = false;
+           respsonseBase64 = JSON.parse(JSON.stringify(responseData));
+
+         //console.log("response here: "+JSON.stringify(responseData));
+         
+          for(let i=0;i<respsonseBase64.OriginalImage.length;i++){
+
+              this.uploadedImg.push(respsonseBase64.OriginalImage[i]);
+              this.uploadedImgProcessed.push(respsonseBase64.proccesedImage[i]);
+              console.log("index: "+respsonseBase64.Index[i]);
+              this.uuid.push(respsonseBase64.Index[i]);
+          }
+        },//code below ensures that if token is invalid or expired user gets sent back to login
+         (err) => {
+          //if (err === 'Unauthorized') { this._router.navigateByUrl('/'); }
+      }
+      );
   }
+
 
 }
