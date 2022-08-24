418bdaf084a1841dde750ffca0c5ddee95e81aad
OmolemoMashigo <93662161+OmolemoMashigo@users.noreply.github.com>
2022-08-24 13:45:28 +0200

    Update live-graphing2.component.ts

--- Front-End/Image-Converter/src/app/live-graphing2/live-graphing2.component.ts
+++ Front-End/Image-Converter/src/app/live-graphing2/live-graphing2.component.ts
@@ -9,15 +9,28 @@ import html2canvas from 'html2canvas';
   templateUrl: './live-graphing2.component.html',
   styleUrls: ['./live-graphing2.component.scss']
 })
+
 export class LiveGraphing2Component implements OnInit {
   myScriptElement: HTMLScriptElement;
   Desmos: any;
-  captureScreenshots(){};
+  
 
   constructor() { 
     this.myScriptElement = document.createElement("script");
     this.myScriptElement.src = "https://www.desmos.com/api/v1.7/calculator.js?apiKey=dcb31709b452b1cf9dc26972add0fda6";
     document.body.appendChild(this.myScriptElement);
+
+    // this.myScriptElement.onload = () => {
+    //   this.Desmos = (<any>window).Desmos;
+    //   console.log('loaded');
+    //   document.getElementsByTagName('head')[0].appendChild(this.myScriptElement);
+    //   var elt = document.getElementById('calculator');
+    //   // var calculator = this.Desmos.GraphingCalculator(elt, { keypad: true, expressions: true, settingsMenu: false, expressionsCollapsed: true });
+    //   var calculator = this.Desmos.GraphingCalculator(elt);      
+    //   calculator.setExpression({});
+       
+    // } 
+    
   }
 
   ngOnInit(): void {
@@ -30,31 +43,43 @@ export class LiveGraphing2Component implements OnInit {
         // var calculator = this.Desmos.GraphingCalculator(elt, { keypad: true, expressions: true, settingsMenu: false, expressionsCollapsed: true });
         var calculator = this.Desmos.GraphingCalculator(elt);      
         calculator.setExpression({});
-        
-
-        // var btnElt = document.getElementById('screenshot-button');
-        // btnElt.addEventListener('click',this.captureScreenshots);
-        
-        
-        
-
+         
     }
 
-  
+    // alert("hey");
 
   }
 
-    getName()
-    {
-      alert("yah saan")
-    }
-    // captureScreenshots () {
-    // img1x.src = calculator.screenshot({
-    //   height: 500,
-    //   width: 500
-    // });
 
 
+  captureScreenshots () {
+    
+    var thumbnail = this.Desmos.calculator.screenshot({
+      width: 200,
+      height: 200,
+      targetPixelRatio: 2
+    });
 
 
+    var img = document.createElement('img');
+    img.src = thumbnail;
+
+    console.log(img.src);
+
+    console.log("huh?");
+    alert("we did it");
+  }
+  
+  
+
+  
 }
+
+
+    
+    
+
+
+
+
+
