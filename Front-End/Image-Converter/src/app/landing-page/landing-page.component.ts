b313e2d2506f5a855f41623f45eb98f9adb88e05
OmolemoMashigo <93662161+OmolemoMashigo@users.noreply.github.com>
2022-09-19 16:29:18 +0200

    routing

--- Front-End/Image-Converter/src/app/landing-page/landing-page.component.ts
+++ Front-End/Image-Converter/src/app/landing-page/landing-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Title, Meta} from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
@@ -8,12 +9,17 @@ import {Title, Meta} from '@angular/platform-browser';
 })
 export class LandingPageComponent implements OnInit {
 
-  constructor(private meta: Meta) { 
+  constructor(private meta: Meta, private _router: Router) { 
 
     meta.updateTag({name:'viewport', content:'width=device-width, initial-scale=1.0'});
   }
 
   ngOnInit(): void {
+  }
+
+  onSubmitCode(){
+    this._router.navigateByUrl('/dashboard');
+    alert("button working");
   }
 
 }
--- Front-End/Image-Converter/src/app/welcome-page/welcome-page.component.ts
+++ Front-End/Image-Converter/src/app/welcome-page/welcome-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Title, Meta} from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-welcome-page',
@@ -8,11 +9,35 @@ import {Title, Meta} from '@angular/platform-browser';
 })
 export class WelcomePageComponent implements OnInit {
 
-  constructor(private meta: Meta) {
+  constructor(private meta: Meta, private _router: Router) {
     meta.updateTag({name:'viewport', content:'width=device-width, initial-scale=1.0'});
    }
 
   ngOnInit(): void {
+  }
+
+  onSubmitUpload(){
+    this._router.navigateByUrl("/dashboard")
+  }
+
+  onSubmitGraphing(){
+    this._router.navigateByUrl("/dashboard")
+  }
+
+  onSubmitHistory(){
+    this._router.navigateByUrl("/dashboard")
+  }
+
+  onSubmitUnrecognized(){
+    this._router.navigateByUrl("/dashboard")
+  }
+
+  onSubmitTracker(){
+    this._router.navigateByUrl("/dashboard")
+  }
+
+  onSubmitRetraining(){
+    this._router.navigateByUrl("/dashboard")
   }
 
 }
