4dcf2f0180e38775b5b8bdb4014052bdc6763e00
Neo Seefane <100659373+neoseefane@users.noreply.github.com>
2022-09-23 14:34:19 +0200

    💥 (Back-end) adding timer to the image smoothing

--- Server/converter/smoothing.py
+++ Server/converter/smoothing.py
@@ -11,13 +11,49 @@ class smoothing:
         self.img = uploaded_image
 
     def clean_noise(self):
+        import time
+        start = time.time()
         sr = cv2.dnn_superres.DnnSuperResImpl_create()
-        path = "converter/EDSR_x4.pb"
-        print('------------------------Loading model----------------------------------------')
+        # Read the desired model
+        path = "EDSR_x4.pb"
         sr.readModel(path)
-        sr.setModel("edsr",1)
+        sr.setModel("edsr",4)
+
+        # Set CUDA backend and target to enable GPU inference
+        sr.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+        sr.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+        
         result = sr.upsample(self.img)
-        print('------------------------DONE Loading model----------------------------------------')
+        end = time.time()
+        print(end - start)
+
+        # sr = cv2.dnn_superres.DnnSuperResImpl_create()
+        # path = "ESPCN_x4.pb"
+        # print('------------------------Loading model1----------------------------------------')
+        # sr.readModel(path)
+        # sr.setModel("espcn",4)
+        # result = sr.upsample(self.img)
+        # print('------------------------DONE Loading model-------------------------------------')
+
+
+        # sr1 = cv2.dnn_superres.DnnSuperResImpl_create()
+        # path = "EDSR_x4.pb"
+        # print('------------------------Loading model 2----------------------------------------')
+        # start = time.time()
+        # sr1.readModel(path)
+        # sr1.setModel("edsr",4)
+        # result1 = sr1.upsample(self.img)
+        # end = time.time()
+        # print(end - start)
+        # print('------------------------DONE Loading model----------------------------------------')
+        
+        # sr2 = cv2.dnn_superres.DnnSuperResImpl_create()
+        # path = "LapSRN_x8.pb"
+        # print('------------------------Loading model3----------------------------------------')
+        # sr2.readModel(path)
+        # sr2.setModel("lapsrn",8)
+        # result2 = sr2.upsample(self.img)
+        # print('------------------------DONE Loading model----------------------------------------')
 
         # kernel = np.array([[0, -1, 0],
         #            [-1, 5,-1],
@@ -33,15 +69,17 @@ class smoothing:
 
         # Using cv2.imshow() method 
         # Displaying the image 
-        # cv2.imshow("Original Image", self.img)
-        # cv2.imshow("sharpened Image", result)
+        cv2.imshow("Original Image", self.img)
+        cv2.imshow("ESDSR_x4", result)
+        # cv2.imshow("eDsr_x4", result1)
+        # cv2.imshow("LapSRN_x8", result2)
         
-        #waits for user to press any key 
-        #(this is necessary to avoid Python kernel form crashing)
-        # cv2.waitKey(0) 
+        # waits for user to press any key 
+        # (this is necessary to avoid Python kernel form crashing)
+        cv2.waitKey(0) 
         
-        #closing all open windows 
-        # cv2.destroyAllWindows() 
+        # closing all open windows 
+        cv2.destroyAllWindows() 
 
         #Resizing the image
         resizedImage = imageResizing(result)
@@ -61,7 +99,7 @@ class smoothing:
         return cleanedImage
 
 if __name__ == '__main__':
-    src = [ 'img.png']
+    src = [ '/Users/neoseefane/Documents/GitHub/Image-Converter/Server/converter/graphs/imagemg8c19s191e1pa1p2answer.png']
     for i in src:
         img = cv2.imread(i)
         object = smoothing(img)
