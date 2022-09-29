6eda725e036826c72aebe1739d8bc14971034811
Phadimane Lerefolo <u19081082@tuks.co.za>
2022-09-29 16:23:34 +0200

    Update test_getUnrecognizedImages.py

--- Server/test/Unit-Tests/test_getUnrecognizedImages.py
+++ Server/test/Unit-Tests/test_getUnrecognizedImages.py
@@ -2,3 +2,16 @@ import os
 from database.mockDatabase import mockDatabase
 from datetime import datetime
 dirname = os.path.dirname(__file__)
+
+def test_GetUnrecognizedImages_GivenAnExistingHistoryRecod_ShouldReturnTrue():
+    #Prepare
+    try:
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        name= "name "+current_time
+        surname= "surname "+current_time
+        email= "email "+current_time
+        password= "password "+current_time
+    except Exception as e:
+        print(f"Test Error : {e}")
+        assert False
