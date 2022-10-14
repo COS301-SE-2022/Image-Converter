124ae16c4e9a747860533527c3ddb3d294df6b22
Neo Seefane <100659373+neoseefane@users.noreply.github.com>
2022-10-14 19:46:05 +0200

    🚧 (Back-end) fixing the /picture api route

--- Server/app.py
+++ Server/app.py
@@ -108,7 +108,7 @@ def upload_image(user):
                 graphType = "This is a "+img_class.graphType
             conv=ConvertFomat()
             conv.covertImgFormat(db_image[4])
-            return jsonify({'image': db_image[4], 'png':conv.getPng(),'jpg':conv.getJpg(), 'graphType': graphType,'id':db_image[0]})
+            return jsonify({'image': db_image[4], 'png':conv.getPng(),'jpg':conv.getJpg(), 'graphType': graphType,'id':db_image[0], 'imageHeight': imageHeight, 'imageWidth': imageWidth})
         else:
             print("picture is None")
             return {'response': 'Picture is None!'},200
