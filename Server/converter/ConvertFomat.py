ba6127a57fc25f13749806f6b01e666f18d10f5f
Neo Seefane <100659373+neoseefane@users.noreply.github.com>
2022-10-22 12:19:33 +0200

    ✨ (Back-end) TIFF and BMP image conversions

--- Server/converter/ConvertFomat.py
+++ Server/converter/ConvertFomat.py
@@ -1,6 +1,5 @@
 from io import BytesIO
 from PIL import Image
-from flask import jsonify
 import requests
 import base64
 
@@ -9,6 +8,8 @@ class ConvertFomat:
     def __init__(self):
         self.png=""
         self.jpg=""
+        self.tif=""
+        self.bmp=""
 
     def covertImgFormat(self,imageLnk):
         
@@ -17,15 +18,24 @@ class ConvertFomat:
         rgb_im = im.convert('RGB')
         rgb_im.save('processedImg.jpg')
         rgb_im.save('processedImg.png')
+        rgb_im.save('processedImg.tiff')
+        rgb_im.save('processedImg.bmp')
+        rgb_im.save('processedImg.jfif')
         print("abv")
 
         with open("processedImg.png", "rb") as img_file:
             convPng = base64.b64encode(img_file.read())
         with open("processedImg.jpg", "rb") as img_file:
             cnvJpg = base64.b64encode(img_file.read())
+        with open("processedImg.tiff", "rb") as img_file:
+            cnvTiff = base64.b64encode(img_file.read())
+        with open("processedImg.bmp", "rb") as img_file:
+            cnvBmp = base64.b64encode(img_file.read())
 
         self.png=str("data:image/png;base64,"+ bytes(convPng).decode('UTF-8'))
         self.jpg=str("data:image/jpg;base64,"+ bytes(cnvJpg).decode('UTF-8'))
+        self.tiff = str("data:image/tiff;base64," + bytes(cnvTiff).decode('UTF-8'))
+        self.bmp = str("data:image/bmp;base64," + bytes(cnvTiff).decode('UTF-8'))
 
     def getPng(self):
         return self.png
