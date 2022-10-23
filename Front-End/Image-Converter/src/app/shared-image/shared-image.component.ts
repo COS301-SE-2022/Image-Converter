29ff389c21ebcc5d3fa2bf7c35da348d08e17e26
Thuso-Khoza <56699783+Thuso-Khoza@users.noreply.github.com>
2022-10-22 21:24:57 -0700

    :construction: functionality to check url parameter

--- Front-End/Image-Converter/src/app/shared-image/shared-image.component.ts
+++ Front-End/Image-Converter/src/app/shared-image/shared-image.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-shared-image',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SharedImageComponent implements OnInit {
 
-  constructor() { }
+  imgage!: string;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+
+    //this check the url parameter
+    this.route.queryParams
+      .subscribe(params => {
+        console.log(params); // { orderby: "price" }
+        this.imgage = params.imgage;
+        console.log(this.imgage); // price
+      }
+    );
+
   }
 
 }
