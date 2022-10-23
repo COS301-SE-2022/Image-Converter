9cabeb6a891e6abe53cb4dd2475eb8688527f874
Thuso-Khoza <56699783+Thuso-Khoza@users.noreply.github.com>
2022-10-22 22:01:43 -0700

    :construction: shared image functionality

--- Front-End/Image-Converter/src/app/shared-image/shared-image.component.ts
+++ Front-End/Image-Converter/src/app/shared-image/shared-image.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute,Router } from '@angular/router';
+import {ConverterService} from './../shared/converter.service';
 
 @Component({
   selector: 'app-shared-image',
@@ -8,21 +9,48 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class SharedImageComponent implements OnInit {
 
-  imgage!: string;
-
-  constructor(private route: ActivatedRoute) { }
+  image!: string;
+  //used for loadig spinner
+  loading=false;
+  imageUrl!: string;//returned image
+  
+  constructor(private _router: Router,private route: ActivatedRoute,private imgService: ConverterService) { }
 
   ngOnInit(): void {
-
+    this.loading=true;
     //this check the url parameter
     this.route.queryParams
       .subscribe(params => {
-        console.log(params); // { orderby: "price" }
-        this.imgage = params.imgage;
-        console.log(this.imgage); // price
+        console.log(params); 
+        this.image = params.image;
+        console.log(this.image); // image
       }
     );
 
+    if(this.image!=undefined && this.image!="")
+    {
+      localStorage.setItem('imageParam',this.image);
+    }
+   
+    //check if user is logged in
+    if(!localStorage.getItem('token') && localStorage.getItem('token')=="")
+    {
+      this._router.navigateByUrl('/');
+    }
+
+    let respsonse:any="";
+    //fetch image
+    this.imgService.getSharedImage(localStorage.getItem('imageParam')!).subscribe(
+      responseData =>{
+        localStorage.setItem('imageParam',"");
+        this.loading = false;
+        respsonse = JSON.parse(JSON.stringify(responseData));
+
+         this.imageUrl=respsonse.Image;
+      },
+       (err) => {
+    }
+    );
   }
 
 }
