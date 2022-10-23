56efc98fd1b261c0f7e72bb90ff22ff6fc8b97f6
Thuso-Khoza <56699783+Thuso-Khoza@users.noreply.github.com>
2022-10-22 21:03:59 -0700

    :sparkles: Displays a list of  images shared with a user

---
+++ Front-End/Image-Converter/src/app/shared-images/shared-images.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shared-images',
+  templateUrl: './shared-images.component.html',
+  styleUrls: ['./shared-images.component.scss']
+})
+export class SharedImagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
