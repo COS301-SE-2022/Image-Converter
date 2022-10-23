e1b84cb56e85990be1f8a994e4e3bdf93ebb9f7d
Thuso-Khoza <56699783+Thuso-Khoza@users.noreply.github.com>
2022-10-22 22:18:27 -0700

    :construction: shared images functionality

--- Front-End/Image-Converter/src/app/shared-images/shared-images.component.ts
+++ Front-End/Image-Converter/src/app/shared-images/shared-images.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ConverterService} from './../shared/converter.service';
 
 @Component({
   selector: 'app-shared-images',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SharedImagesComponent implements OnInit {
 
-  constructor() { }
+  uuid:BigInteger[]=[];
+  //used for loadig spinner
+  loading=false;
+   //holds list of images
+  images: string[] = [];
+
+  constructor(private imgService: ConverterService) { }
 
   ngOnInit(): void {
+
+    this.loading = true;
+    let respsonse:any="";
+      this.imgService.getListSharedImages("").subscribe(
+        responseData =>{
+          this.loading = false;
+          respsonse = JSON.parse(JSON.stringify(responseData));
+          for(let i=0;i<respsonse.OriginalImage.length;i++){
+
+              this.images.push(respsonse.OriginalImage[i]);
+              this.uuid.push(respsonse.Index[i]);
+          }
+        },
+         (err) => {
+         
+      }
+      );
   }
 
+  imageClick(index:any)
+  {
+
+  }
 }
